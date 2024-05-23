1bc203b01281da966664d48b97000b74c4acad8f
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2024-05-23 13:49:53 +0300

    #1 feat: Update the packeges

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { UploadModule } from './upload/upload.module';
+import { FileModule } from './file/file.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://181076:pyys2zOJoUDQCDFt@saz.wnpqbd4.mongodb.net/'),
+    UsersModule,
+    UploadModule,
+    FileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
