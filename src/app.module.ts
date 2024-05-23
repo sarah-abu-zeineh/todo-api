08ae58374a599c7d06c4cc059d1c2dcc4648f384
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2024-05-23 11:44:41 +0300

    #1 feat: Add User Model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://181076:pyys2zOJoUDQCDFt@saz.wnpqbd4.mongodb.net/'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+    @Prop()
+    name: string;
+
+    @Prop({ required: true, unique: true })
+    email: string;
+
+    @Prop({ required: true })
+    password: string;
+
+    @Prop({ default: Date.now })
+    userImg?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
