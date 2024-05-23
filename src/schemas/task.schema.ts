37f1d78355da445b7706fd417c3f13c5748cf571
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2024-05-23 11:47:08 +0300

    #1 feat: Add Task Model

---
+++ src/schemas/task.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Task extends Document {
+    @Prop()
+    title: string;
+
+    @Prop({ required: true })
+    desc: string;
+
+    @Prop({ required: true })
+    status: string;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
