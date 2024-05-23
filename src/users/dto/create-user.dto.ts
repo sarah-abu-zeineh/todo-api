07d8b04a003864883c9a972a1065d1c81c7a70c8
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2024-05-23 20:07:24 +0300

    #1 feat: update user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 import {IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator';
 
-export class CreateUserDto {
+export class CreateUserDto {    
     @IsString()
     @IsNotEmpty()
     name: string;
