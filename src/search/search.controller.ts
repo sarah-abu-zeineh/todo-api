dd044425cb5a4d7df50e5318447d9fe1b8e526f5
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2024-05-23 23:29:22 +0300

    feat: Add state filter to searchUserTasks for filtering tasks by status in addition to title and description

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -10,7 +10,7 @@ export class SearchController {
   constructor(private readonly searchService: SearchService) { }
 
   @Get()
-  async search(@Req() request: Request, @Query('query') query: string) {
-    return this.searchService.searchUserTasks(request['user'], query);
+  async search(@Req() request: Request, @Query('query') query: string, @Query('state') state: string) {
+    return this.searchService.searchUserTasks(request['user'], query, state);
   }
 }
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { filter } from 'rxjs';
+import { TaskState } from 'src/enums/taskState.enum';
 import { Task } from 'src/schemas/task.schema';
 import { User } from 'src/schemas/user.schema';
 
@@ -9,9 +11,9 @@ export class SearchService {
   constructor(
     @InjectModel(User.name) private userModel: Model<User>,
     @InjectModel(Task.name) private taskModel: Model<Task>,
-  ) {}
+  ) { }
 
-  async searchUserTasks(userData: string, searchTerm: string): Promise<Task[]> {
+  async searchUserTasks(userData: string, searchTerm: string, state: string): Promise<Task[]> {
     const user = await this.userModel.findById(userData['id']).populate({
       path: 'tasks',
       model: this.taskModel,
@@ -22,8 +24,12 @@ export class SearchService {
     }
 
     const regex = new RegExp(searchTerm, 'i');
-
-    const filteredTasks = user.tasks.filter(task => regex.test(task.title) || regex.test(task.desc));
+    let filteredTasks = user.tasks.filter(task => regex.test(task.title) || regex.test(task.desc));
+    
+    console.log(state);
+    if (state == TaskState.COMPLETED || state == TaskState.PENDING) {
+      filteredTasks = filteredTasks.filter(task => task.status === state);
+    }
 
     return filteredTasks;
   }
