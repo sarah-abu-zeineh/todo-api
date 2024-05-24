338d5d6b817617818f1b6b37113a2fbb1ec8ac3b
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2024-05-24 03:05:18 +0300

    #3 refactor: Remove unnecessary Guard

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
 import { SearchService } from './search.service';
-import { AuthGuard } from '../auth/auth.guard';
 import { Request } from 'express';
-import { Task } from 'src/schemas/task.schema';
 
-@UseGuards(AuthGuard)
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) { }
